44da0acbc8f4512178fc0c344cd4bdd85e118e56
Daniel Gongo <dan_liveira@outlook.com>
2022-10-17 12:32:43 -0300

    Projeto jogo advinhaçãoi

---
+++ pooJogoAdivinhacao/NovaPergunta.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooJogoAdivinhacao
+{
+    public class NovaPergunta : PerguntaJogo
+    {
+
+        public NovaPergunta():base()
+        {
+            this.Dica = "";
+        }
+
+        public NovaPergunta(string pergunta, string resposta, string dica) : base(pergunta, resposta){
+
+            this.Dica = dica; //Quando se coloca os parâmetros na base, não precisa colocar dentro do construtor
+        }
+
+        private string dica;
+        public string Dica {
+
+            get { return dica; }
+           
+            set
+            {
+                dica = value.ToUpper();
+            }
+                
+        }
+    }
+}
---
+++ pooJogoAdivinhacao/PerguntaJogo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooJogoAdivinhacao
+{
+    public class PerguntaJogo
+    {
+        public PerguntaJogo()
+        {
+            this.Pergunta = "";
+            this.Resposta = "";
+
+        }
+
+        public PerguntaJogo(string pergunta, string resposta)
+        {
+            this.Pergunta = pergunta;
+            this.Resposta = resposta;
+        }
+
+        private string pergunta;
+        public string Pergunta {
+            get { return this.pergunta; }
+
+            set 
+            {
+                pergunta = value.ToUpper();
+            } 
+        }
+
+        private string resposta;
+        public string Resposta
+        {
+            get { return this.resposta; }
+
+            set
+            {
+                resposta = value.ToUpper();
+            }
+        }
+    }
+}
